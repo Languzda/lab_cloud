fa6c0805f56febecd7cc405b21051baa6862fd1c
Languzda <langusda@gmail.com>
2025-03-26 12:33:17 +0100

    fix autoincreament, add gitignore

--- db/data/import.sql
+++ db/data/import.sql
@@ -1,16 +1,16 @@
 -- Tworzenie tabeli, jeśli nie istnieje
 CREATE TABLE IF NOT EXISTS games (
-    rank INTEGER PRIMARY KEY,
-    name TEXT,
+    rank SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
     platform TEXT,
     year INTEGER,
     genre TEXT,
     publisher TEXT,
-    na_sales NUMERIC,
-    eu_sales NUMERIC,
-    jp_sales NUMERIC,
-    other_sales NUMERIC,
-    global_sales NUMERIC
+    na_sales FLOAT,
+    eu_sales FLOAT,
+    jp_sales FLOAT,
+    other_sales FLOAT,
+    global_sales FLOAT
 );
 
 -- Import danych z CSV
@@ -18,3 +18,5 @@ COPY games(rank, name, platform, year, genre, publisher, na_sales, eu_sales, jp_
 FROM '/docker-entrypoint-initdb.d/vgsales_cleaned.csv'
 DELIMITER ','
 CSV HEADER;
+
+SELECT setval('games_rank_seq', COALESCE((SELECT MAX(rank) FROM games), 0)+1, false);
